ad324f3b246d974ee4017133ed5c7131ba0d64cd
kauã reis <bitokinhajf@gmail.com>
2021-01-14 04:54:40 -0300

    1-commit

---
+++ index.js
@@ -0,0 +1,48 @@
+var parametro = true
+function funcaosobedesce() {
+    if (parametro == true) {
+        document.getElementById('link').href = '#rodape';
+        document.getElementById('seta').style.transform = "rotate(0deg)";
+        parametro = false
+    } else {
+        document.getElementById('link').href = '#astroworld';
+        document.getElementById('seta').style.transform = "rotate(180deg)";
+        parametro = true
+    }
+}
+function funcaorandom() {
+    destino = Math.floor(Math.random() * 10) + 1;
+    if (destino == 0) {
+        document.getElementById('mensagem').
+    }
+    if (destino == 1) {
+        document.getElementById('mensagem').  
+    }
+    if (destino == 2) {
+        document.getElementById('mensagem').  
+    }
+    if (destino == 3) {
+        document.getElementById('mensagem').  
+    }
+    if (destino == 4) {
+        document.getElementById('mensagem').  
+    }
+    if (destino == 5) {
+        document.getElementById('mensagem').  
+    }
+    if (destino == 6) {
+        document.getElementById('mensagem').  
+    }
+    if (destino == 7) {
+        document.getElementById('mensagem').  
+    }
+    if (destino == 8) {
+        document.getElementById('mensagem').  
+    }
+    if (destino == 9) {
+        document.getElementById('mensagem').  
+    }
+    if (destino == 10) {
+        document.getElementById('mensagem').  
+    }
+}
