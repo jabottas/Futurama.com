a1601fa4064aacccc57a5696e18313733136cf14
kauã reis <bitokinhajf@gmail.com>
2021-02-01 02:58:03 -0300

    7-commit

--- index.js
+++ index.js
@@ -17,37 +17,37 @@ function funcaorandom() {
 function funcaorandom() {
     destino = Math.floor(Math.random() * 10) + 1;
     if (destino == 0) {
-        document.getElementById('mensagem').
+        document.getElementById('mensagem').innerHTML="teste1";
     }
     if (destino == 1) {
-        document.getElementById('mensagem').  
+        document.getElementById('mensagem').innerHTML="teste2";  
     }
     if (destino == 2) {
-        document.getElementById('mensagem').  
+        document.getElementById('mensagem').innerHTML="teste3";  
     }
     if (destino == 3) {
-        document.getElementById('mensagem').  
+        document.getElementById('mensagem').innerHTML="teste4"; 
     }
     if (destino == 4) {
-        document.getElementById('mensagem').  
+        document.getElementById('mensagem').innerHTML="teste5";  
     }
     if (destino == 5) {
-        document.getElementById('mensagem').  
+        document.getElementById('mensagem').innerHTML="teste6";  
     }
     if (destino == 6) {
-        document.getElementById('mensagem').  
+        document.getElementById('mensagem').innerHTML="teste7";  
     }
     if (destino == 7) {
-        document.getElementById('mensagem').  
+        document.getElementById('mensagem').innerHTML="teste8";  
     }
     if (destino == 8) {
-        document.getElementById('mensagem').  
+        document.getElementById('mensagem').innerHTML="teste9";  
     }
     if (destino == 9) {
-        document.getElementById('mensagem').  
+        document.getElementById('mensagem').innerHTML="teste10";  
     }
     if (destino == 10) {
-        document.getElementById('mensagem').  
+        document.getElementById('mensagem').innerHTML="teste11";  
     }
 }
 */
